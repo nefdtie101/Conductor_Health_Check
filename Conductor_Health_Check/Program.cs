14dff3b0605903300bbc3b1ef9f3b6dce6a0e9c8
johannnefdr <jnefdt@tutulearn.com>
2025-09-06 16:21:54 +0200

    Enable Swagger and SwaggerUI for all environments.

--- Conductor_Health_Check/Program.cs
+++ Conductor_Health_Check/Program.cs
@@ -27,13 +27,8 @@ builder.Services.AddHostedService<TaskService>();
 
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapHealthChecks("/health");
 
